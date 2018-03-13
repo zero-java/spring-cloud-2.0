9af60b4eead9e971ed1fa58510370f37c70190fe
scvzerng <842494771@qq.com>
2018-03-13 20:00:34 +0800

    eureka server

---
+++ eureka-server/src/main/java/com/zero/scvzerng/EurekaServer.java
@@ -0,0 +1,17 @@
+package com.zero.scvzerng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author scvzerng
+ * @date 2018-2018/3/13-19:43
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServer {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServer.class,args);
+    }
+}
